960e5b5c38b754734a8f207984e80012e33552a8
Matthew Webb <mwebb2321@gmail.com>
2021-04-27 16:30:57 -0700

    added db.js file

--- public/db.js
+++ public/db.js
@@ -13,4 +13,72 @@ request.onupgradeneeded = function (e) {
   console.log(`DB updated from ${oldVersion} to ${newVersion}`);
 
   db = e.target.result;
+
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore("BudgetStore", { autoIncrement: true });
+  }
 };
+
+request.onerror = function (e) {
+  console.log(`Ya messed up! ${e.target.errorCode}`);
+};
+
+function checkDataBase() {
+  console.log("Check DB involved");
+
+  // Open transaction on BudgetStore DB
+  let transaction = db.transaction(["BudgetStore"], "readwrite");
+
+  // Access BudgetStore Object
+  const store = transaction.objectStore("BudgetStore");
+
+  // Get All Records from Store And To Set A Variable
+  const getAll = store.getAll();
+
+  // If Request Was Successful
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json)
+        .then((res) => {
+          if (res.length !== 0) {
+            transaction = db.transaction(["BudgetStore"], "readwrite");
+
+            currentStore.clear();
+            console.log("Clearing store!");
+          }
+        })
+        .catch((err) => {
+          res.json(err);
+        });
+    }
+  };
+}
+
+request.onsuccess = function (e) {
+  console.log("success");
+  db = e.target.result;
+
+  if (navigator.onLine) {
+    console.log("Backend online!");
+    checkDataBase();
+  }
+};
+
+const saveRecord = (record) => {
+  console.log("Save record invoked");
+  const transaction = db.transaction(["BudgetStore"], "readwrite");
+
+  const store = transaction.objectStore("BudgetStore");
+
+  store.add(record);
+};
+
+window.addEventListener("online", checkDataBase);
